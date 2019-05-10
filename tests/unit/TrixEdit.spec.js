61d50f92ee3d5f3a6b00ec4c24bda349aaa0628d
Tony Trinh <tony19@gmail.com>
2019-05-10 06:28:40 -0500

    Add spec for TrixEdit

---
+++ tests/unit/TrixEdit.spec.js
@@ -0,0 +1,20 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import TrixEdit from '@/components/TrixEdit.vue'
+
+const localVue = createLocalVue()
+localVue.config.ignoredElements = ['trix-editor']
+
+describe('TrixEdit', () => {
+  describe('value prop', () => {
+    it('renders text when value is set', () => {
+      const wrapper = mount(TrixEdit, {
+        localVue,
+        propsData: {
+          value: 'This is a test'
+        }
+      })
+
+      expect(wrapper.emitted().input).toEqual('This is a test')
+    })
+  })
+})
