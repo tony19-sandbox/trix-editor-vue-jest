f3bb8be3f06808ef9252bcd3c55cea1dbb9ded47
Tony Trinh <tony19@gmail.com>
2019-05-10 06:41:23 -0500

    Add mutationobserver-shim + window.getSelection stub to jest setup

--- jest.config.js
+++ jest.config.js
@@ -19,6 +19,7 @@ module.exports = {
   snapshotSerializers: [
     'jest-serializer-vue'
   ],
+  setupTestFrameworkScriptFile: '<rootDir>/jest-setup.js',
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
