6d87bb40900d0548056fae8ea1237250446fbccf
Tony Trinh <tony19@gmail.com>
2019-05-10 06:30:28 -0500

    Move init code to trix-initialize handler

--- src/components/TrixEdit.vue
+++ src/components/TrixEdit.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="trix">
-    <trix-editor></trix-editor>
+    <trix-editor @trix-initialize="onInit" />
   </div>
 </template>
 
@@ -9,18 +9,20 @@ import 'trix'
 
 export default {
   props: ['value'],
-  mounted() {
-    let el = this.$refs.trix.getElementsByTagName('trix-editor')[0]
-    // HACK: change the URL field in the link dialog to allow non-urls
-    let toolbar = this.$refs.trix.getElementsByTagName('trix-toolbar')[0]
-    toolbar.querySelector('[type=url]').type = 'text'
+  methods: {
+    onInit(e) {
+      let el = e.target
+      // HACK: change the URL field in the link dialog to allow non-urls
+      let toolbar = this.$refs.trix.getElementsByTagName('trix-toolbar')[0]
+      toolbar.querySelector('[type=url]').type = 'text'
 
-    // insert content
-    el.editor.insertHTML(this.value)
+      // insert content
+      el.editor.insertHTML(this.value)
 
-    el.addEventListener('trix-change', e => {
-      this.$emit('input', e.target.innerHTML)
-    })
+      el.addEventListener('trix-change', e => {
+        this.$emit('input', e.target.innerHTML)
+      })
+    }
   }
 }
 </script>
