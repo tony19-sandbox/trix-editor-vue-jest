24b3fe0e9192f1fdf70ffb1abf34d50cb372b22a
Tony Trinh <tony19@gmail.com>
2019-05-10 06:27:12 -0500

    Add TrixEdit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TrixEdit />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TrixEdit from './components/TrixEdit.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TrixEdit
   }
 }
 </script>
---
+++ src/components/TrixEdit.vue
@@ -0,0 +1,25 @@
+<template>
+  <div ref="trix">
+    <trix-editor></trix-editor>
+  </div>
+</template>
+
+<script>
+import 'trix'
+
+export default {
+  mounted() {
+    let el = this.$refs.trix.getElementsByTagName('trix-editor')[0]
+    // HACK: change the URL field in the link dialog to allow non-urls
+    let toolbar = this.$refs.trix.getElementsByTagName('trix-toolbar')[0]
+    toolbar.querySelector('[type=url]').type = 'text'
+
+    // insert content
+    el.editor.insertHTML(this.value)
+
+    el.addEventListener('trix-change', e => {
+      this.$emit('input', e.target.innerHTML)
+    })
+  }
+}
+</script>
